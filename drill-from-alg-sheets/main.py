8754efa0190a81ca74775cb361e9f843797fceeb
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-10-25 11:01:17 -0700

    Move to drill-from-alg-sheets

--- drill-from-alg-sheets/main.py
+++ drill-from-alg-sheets/main.py
@@ -30,17 +30,15 @@ class Table:
         row = [row for row in self.rows if row[0] == t2][0]
         return row[i] or None
 
-def is_simple(alg):
+def num_setup_moves(alg):
     if ':' not in alg:
-        return True
-    setup_length = len(alg.split(':')[0].split())
-    return setup_length < 2
+        return 0
+    return len(alg.split(':')[0].split())
 
 # filename = 'daniel-lin.csv'  # TODO: `targets` is hard-coded to max-hilliard.csv
 filename = 'max-hilliard.csv'
 maxtable = Table(filename)
-simple_cases = []
-num_total_cases = 0
+cases_by_setup_length = collections.defaultdict(list)
 n = 4
 
 Case = collections.namedtuple('Case', 'targets alg')
@@ -50,14 +48,14 @@ for t1 in targets:
         alg = maxtable.get(t1, t2)
         if not alg:
             continue
-        num_total_cases += 1
-        if is_simple(alg):
-            simple_cases.append(Case(t1 + ' ' + t2, alg))
+        setup_length = num_setup_moves(alg)
+        cases_by_setup_length[setup_length].append(Case(t1 + ' ' + t2, alg))
 
 print(f"{filename}:")
-print(f"  Simple cases: {len(simple_cases)}")
-print(f"  Total cases: {num_total_cases}")
+for n in sorted(cases_by_setup_length):
+    print(f"  Cases with {n} setup moves: {len(cases_by_setup_length[n])}")
+print("4 random 0-setup cases:")
 print()
 
-for i in range(n):
-    print(random.choice(simple_cases).targets)
+for i in range(4):
+    print(random.choice(cases_by_setup_length[0]).targets)
