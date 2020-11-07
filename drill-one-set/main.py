5ed1a60920ee560a471435fb641c65b156b77629
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-11-06 17:39:06 -0800

    Drill one set

---
+++ drill-one-set/main.py
@@ -0,0 +1,34 @@
+import random
+
+A_TO_X = 'ABCDEFGHIJKLMNOPQRSTUVWX'
+BUFFER = 'CJM'
+PIECES = ['ARE','BQN','DIF','ULG','VKP','WOT','XSH']
+
+
+def main():
+    letters = without(A_TO_X, BUFFER)
+    x = input('Drill which set? (e.g. B) ')
+    pairs = []
+    for y in letters:
+        if same_piece(x, y):
+            continue
+        pairs.append(random.choice([x + y, y + x]))
+    random.shuffle(pairs)
+    print()
+    for p in pairs:
+        print(p)
+
+
+def without(s, remove):
+    return ''.join(c for c in s if c not in remove)
+
+def same_piece(x, y):
+    return piece_index(x) == piece_index(y)
+
+def piece_index(x):
+    for i, piece in enumerate(PIECES):
+        if x in piece:
+            return i
+    raise ValueError('Piece not found')
+
+main()
